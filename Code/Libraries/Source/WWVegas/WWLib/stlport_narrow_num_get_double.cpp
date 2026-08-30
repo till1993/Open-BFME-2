ffc60a3d6d285d51948f7dbf0b77d5c39f701cf4
Till Voß <till.voss@mailbox.org>
2026-08-30 14:48:17 +0200

    Match narrow STLport sign scanner

--- Code/Libraries/Source/WWVegas/WWLib/stlport_narrow_num_get_double.cpp
+++ Code/Libraries/Source/WWVegas/WWLib/stlport_narrow_num_get_double.cpp
@@ -124,19 +124,61 @@ private:
 };
 
 template <class CharT, class Traits>
+class basic_streambuf
+{
+public:
+	int sbumpc();
+};
+
+template <class CharT, class Traits>
 class istreambuf_iterator
 {
 public:
 	bool equal(const istreambuf_iterator &) const;
+	__declspec(dllimport) __forceinline CharT operator*() const
+	{
+		_M_getc();
+		return _M_c;
+	}
+	__declspec(dllimport) __forceinline istreambuf_iterator &operator++()
+	{
+		_M_buf->sbumpc();
+		_M_have_c = false;
+		return *this;
+	}
 
 private:
-	void *_M_buf;
-	CharT _M_c;
-	unsigned char _M_eof;
-	unsigned char _M_have_c;
+	void _M_getc() const;
+
+	basic_streambuf<CharT, Traits> *_M_buf;
+	mutable CharT _M_c;
+	mutable unsigned char _M_eof;
+	mutable unsigned char _M_have_c;
 };
 
 typedef istreambuf_iterator<char, char_traits<char> > narrow_iterator;
+
+template <class InputIter, class CharT>
+InputIter __cdecl __copy_sign(
+		InputIter first, InputIter last, narrow_string &buffer,
+		CharT plus, CharT minus)
+{
+	if (!first.equal(last))
+	{
+		CharT character = *first;
+		if (character == plus)
+			++first;
+		else if (character == minus)
+		{
+			buffer.push_back('-');
+			++first;
+		}
+	}
+	return first;
+}
+
+template narrow_iterator __copy_sign<narrow_iterator, char>(
+		narrow_iterator, narrow_iterator, narrow_string &, char, char);
 
 template <class InputIter, class CharT>
 bool __cdecl _M_read_float(
